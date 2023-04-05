89689df906c6a66cc3ec1f39c5233a9aa7e50f7c
forward_line <forward_line@mail.ru>
2023-04-05 12:39:38 +0500

    Решение Задачи №2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе чисдо");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+            {
+                Console.WriteLine($"max = {a}");
+            }
+else
+            {
+                Console.WriteLine($"max = {b}");
+            }
