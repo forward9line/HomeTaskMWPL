8805750d86ef7f404f16a114d773c027de0213df
forward_line <forward_line@mail.ru>
2023-04-05 21:18:18 +0500

    Zadacha 6 решение

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число, проверим являтся ли оно чётным:");
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 1 | a%2 == -1)
+                    {
+                        Console.WriteLine("Нет");
+                    }
+else
+    {
+        Console.WriteLine("Да");
+    }
